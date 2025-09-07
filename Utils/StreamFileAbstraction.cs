534887bdcb9a2a33ece155a644faaef370c6fe8f
Putra3340 <putraerer3@gmail.com>
2025-09-07 18:11:32 +0700

    Add StreamFileAbstraction class for handling audio file streams
    - Implemented StreamFileAbstraction to manage read and write streams for audio files.
    - Ensured compatibility with TagLib's IFileAbstraction interface.
    - Added a method to close streams without disposing, allowing ASP.NET Core to manage them.

---
+++ Utils/StreamFileAbstraction.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using TagLib;
+namespace Floaty_Music.Utils
+{
+
+
+    public class StreamFileAbstraction : TagLib.File.IFileAbstraction
+    {
+        public StreamFileAbstraction(string name, Stream stream)
+        {
+            Name = name;
+            ReadStream = stream;
+            WriteStream = stream;
+        }
+
+        public string Name { get; }
+
+        public Stream ReadStream { get; }
+
+        public Stream WriteStream { get; }
+
+        public void CloseStream(Stream stream)
+        {
+            // don't dispose here, ASP.NET Core will manage the stream
+        }
+    }
+
+}
